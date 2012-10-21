36208f0fa59abfa6adf8a467dc48d78072bb17e3
Kyle Corbitt <kylecorbitt@gmail.com>
2012-10-20 18:18:37 -0600

    working static directory

--- billreader/billreader/settings.py
+++ billreader/billreader/settings.py
@@ -73,11 +73,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/dotcloud/code/static/'
+STATIC_ROOT = '/home/dotcloud/current/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://billreader-kyle.dotcloud.com/'
+STATIC_URL = 'http://billreader-kyle.dotcloud.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
